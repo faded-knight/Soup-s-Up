8d08903da921be46d4150493938b02c9bdb24ffc
Callum Stirrup-Prazak <s170726@students.aie.edu.au>
2019-03-25 16:30:37 +1100

    Reimplemented Chef Texture, Animation and Scene Asset
    Signed-off-by: Callum Stirrup-Prazak <s170726@students.aie.edu.au>

---
+++ Soup's Up/Assets/Script/MenuManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
